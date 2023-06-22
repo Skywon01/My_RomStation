237fb08965fc6978dad2ab28b5a8ba2a492d37b0
Skywon <doublet.yann@orange.fr>
2023-06-22 17:18:31 +0200

    Page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// ...
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function home(): Response
+    {
+
+
+        return $this->render('home.html.twig', [
+
+        ]);
+    }
+}
